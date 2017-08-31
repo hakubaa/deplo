aad076bb41241374b51c9088d22e7c5fa9113931
kuba <jakub.agatowski@gmail.com>
2017-08-31 23:42:19 +0200

    Add models: User, Role

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,25 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from . import main
+from app import db
+from app.models import User
+
 
 @main.route("/", methods=["GET"])
 def index():
     return render_template("main/index.html")
+
+@main.route("/users", methods=["GET"])
+def users():
+    users = db.session.query(User).all()
+    return render_template("main/users.html", users=users)
+
+@main.route("/users/<user_name>", methods=["GET"])
+def new_user(user_name):
+    try:
+        user = User(username=user_name)
+        db.session.add(user)
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+    return redirect(url_for("main.users"))
--- config.py
+++ config.py
@@ -5,7 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "3K5JDKFKJD345345"
-
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     @staticmethod
     def init_app(app):
         pass
